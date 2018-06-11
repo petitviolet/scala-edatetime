fc2bcc937c98eb46f321f8ef672fefe6abaa8aa8
petitviolet <violethero0820@gmail.com>
2018-06-12 08:21:22 +0900

    rename packages

--- modules/example/src/main/scala/net/petitviolet/time/example/example.scala
+++ modules/example/src/main/scala/net/petitviolet/time/example/example.scala
@@ -1,16 +1,19 @@
-package net.petitviolet.time.cache
+package net.petitviolet.time.example
 
 import java.time.ZoneId
 
-import net.petitviolet.time.GlobalEDateTimeSettings
+import net.petitviolet.time.cache.CachedDateTime
+import net.petitviolet.time.{EDateTime, GlobalEDateTimeSettings}
 
 object example extends App {
   println("=====================")
+  println(s"${EDateTime.now()}")
   CachedDateTime.init()
   println(s"now = ${CachedDateTime.now()}")
   GlobalEDateTimeSettings.zoneId = ZoneId.of("America/New_York")
   Thread.sleep(3000L)
   println(s"now = ${CachedDateTime.now()}")
+  println(s"${EDateTime.now()}")
   println("=====================")
   CachedDateTime.shutdown()
 }
